48fd945b4c7633a56b8f8be5e8dac3c508d86fa6
gilperopiola <ferra.main@gmail.com>
2020-04-29 11:05:51 -0300

    initial commit

---
+++ EnemyAppearFromCenterManager.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAppearFromCenterManager : MonoBehaviour {
+
+    float completion = 0;
+
+    float timeStill = 1.5f;
+    float timeStillOut = 2.8f;
+    float timer = 0f;
+
+    int bulletsToFire = 3;
+    int nShot = 0;
+    float[] timesToShoot;
+
+    SmoothProgressor progressor;
+    SmoothProgressor progressorOut;
+    Material material;
+
+    void Start() {
+        progressor = new SmoothProgressor();
+        progressorOut = new SmoothProgressor();
+
+        timesToShoot = new float[bulletsToFire];
+        for (int i = 0; i < bulletsToFire; i++) {
+            timesToShoot[i] = (timeStill / (bulletsToFire + 1)) * (i + 1);
+        }
+
+        this.StartFadeIn();
+
+        material = gameObject.GetComponent<SpriteRenderer>().material;
+    }
+
+    void StartFadeIn() {
+        nShot = 0;
+        progressor.StartTransition(0, 1.1f, 30);
+        progressor.InvertSlope();
+    }
+
+    void StartFadeOut() {
+        progressorOut.StartTransition(0, -1.1f, 30);
+    }
+
+    bool IsFadedOut() {
+        return completion <= 0.1f;
+    }
+
+    void FixedUpdate() {
+        if (progressor.active) {
+            completion += progressor.Progress();
+
+            material.SetFloat("_Completion", completion);
+        } else if (!progressorOut.active) {
+            timer += Time.fixedDeltaTime;
+
+            //waiting to appear
+            if (this.IsFadedOut()) {
+                if (timer >= timeStillOut) {
+                    timer = 0;
+                    StartFadeIn();
+                }
+                //waiting to disappear / shooting
+            } else {
+
+                for (int i = nShot; i < timesToShoot.Length; i++) {
+                    if (timer >= timesToShoot[i]) {
+
+                        BulletCreator.FromTo(
+                            gameObject.transform.position,
+                            GameObject.Find("Player").transform.position,
+                            1,
+                            ShootingPatterns.One,
+                            false,
+                            5f,
+                            true
+                        );
+
+                        nShot++;
+                    }
+                }
+
+                if (timer >= timeStill) {
+                    timer = 0;
+                    StartFadeOut();
+                }
+            }
+        } else {
+            completion += progressorOut.Progress();
+
+            material.SetFloat("_Completion", completion);
+
+            //finished cycle
+            if (!progressorOut.active) {
+                transform.position = PositionGenerator.NearAndInWater(GameObject.Find("Player").transform.position);
+            }
+        }
+    }
+}
---
+++ WaterDisplacementManager.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterDisplacementManager : MonoBehaviour {
+
+    float displacement = 0f;
+    float step = 0.0032f;
+
+    int status = 0; //0 = rising; 1 = waiting; 2 = descending; 3 = waiting;
+    float timer = 0;
+
+    float minValue = -0.22f;
+    float waitingTime1 = 0.08f;
+    float waitingTime2 = 0.09f;
+
+    Material waterDisplacementMat;
+
+    void Start() {
+        waterDisplacementMat = (Material)Resources.Load("Materials/Water Displacement", typeof(Material));
+    }
+
+    void FixedUpdate() {
+
+        if (status == 0) {
+            displacement -= step;
+
+            if (displacement <= minValue) {
+                status = 1;
+                timer = 0;
+            }
+        }
+
+        if (status == 1) {
+            timer += step;
+
+            if (timer >= waitingTime1) {
+                status = 2;
+                timer = 0;
+            }
+        }
+
+        if (status == 2) {
+            displacement += step;
+
+            if (displacement >= 0) {
+                status = 3;
+                timer = 0;
+            }
+        }
+
+        if (status == 3) {
+            timer += step;
+
+            if (timer >= waitingTime2) {
+                status = 0;
+                timer = 0;
+            }
+        }
+
+        waterDisplacementMat.SetFloat("_DisplacementY", displacement);
+    }
+}
